5600ae281f363e6d06929a70bd19acccd3a4875b
ngendakuriyoleonce <ndendakuriyoleonce75@gmail.com>
2026-07-07 18:31:11 +0400

    Create Product model and migration

---
+++ app/Models/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    protected $fillable = ['name', 'slug', 'description', 'image', 'price', 'stock', 'category_id', 'brand_id'];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
+}
