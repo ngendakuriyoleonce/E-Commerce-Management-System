32a1b6abf8d0f6b6601602913babdc7f40cc536e
ngendakuriyoleonce <ndendakuriyoleonce75@gmail.com>
2026-07-07 19:34:59 +0400

    Add Address model and migration

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -3,10 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Address extends Model
 {
     protected $fillable = [
         'label', 'street', 'city', 'state', 'zip_code', 'country', 'is_primary',
     ];
+
+    public function shippingOrders(): HasMany
+    {
+        return $this->hasMany(Order::class, 'shipping_address_id');
+    }
+
+    public function billingOrders(): HasMany
+    {
+        return $this->hasMany(Order::class, 'billing_address_id');
+    }
 }
